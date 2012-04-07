e15f34492e35d063ba25c96731c24c6283085ca3
Marc Hoersken <info@marc-hoersken.de>
2012-04-07 23:59:48 +0200

    Made it possible to execute unittests from inside subfolder

--- unittests/ufxtract.py
+++ unittests/ufxtract.py
@@ -1,7 +1,11 @@
 import unittest
 
+if __name__ == '__main__':
+    import sys
+    sys.path.append('..')
+
 from ufxtract_hcalendar1 import hCalendar1
 from ufxtract_hcalendar3 import hCalendar3
     
 if __name__ == '__main__':
-    unittest.main()    
+    unittest.main()
